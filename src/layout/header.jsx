988894a54b044ef8945efd3fc5ec16311493d091
vvvarra <120254028+vvvarra@users.noreply.github.com>
2024-03-04 16:22:31 +0300

    Add files via upload

---
+++ src/layout/header.jsx
@@ -0,0 +1,14 @@
+function Header(){
+    return <nav className="#00897b teal darken-1">
+        <div className="container">
+            <div className="nav-wrapper">
+                <a href="!#" className="brand-logo">React Movies</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="!#">Repository</a></li>
+                </ul>
+            </div> 
+
+        </div>
+  </nav>
+}
+export { Header };
